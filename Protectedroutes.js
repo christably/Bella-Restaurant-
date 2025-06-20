3ac3a9598ff926cb3e32a4b78f59894aafd90e7f
Christabel Aidoo <113652194+christably@users.noreply.github.com>
2025-06-20 14:39:44 +0000

    Update Protectedroutes.js

--- Protectedroutes.js
+++ Protectedroutes.js
@@ -1,8 +1,17 @@
-
 // routes/protectedRoute.js
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
+const { authorizeRoles } = require('../middleware/roleMiddleware');
+
+router.get('/admin-area', protect, authorizeRoles('admin'), (req, res) => {
+  res.json({ message: `Welcome Admin ${req.user.username}` });
+});
+
+router.get('/vendor-area', protect, authorizeRoles('vendor', 'admin'), (req, res) => {
+  res.json({ message: `Hello Vendor ${req.user.username}` });
+});
+
 
 router.get('/dashboard', protect, (req, res) => {
   res.json({
