fcf83fa24f6e294d5ba53bab08d79060b0a15205
Christabel Aidoo <113652194+christably@users.noreply.github.com>
2025-06-20 13:14:10 +0000

    Create ERRO.js

---
+++ ERRO.js
@@ -0,0 +1,8 @@
+at Module.load (node:internal/modules/cjs/loader:1465:32)
+    at Function._load (node:internal/modules/cjs/loader:1282:12)
+    at TracingChannel.traceSync (node:diagnostics_channel:322:14)
+    at wrapModuleLoad (node:internal/modules/cjs/loader:235:24)
+    at Module.require (node:internal/modules/cjs/loader:1487:12)
+
+Node.js v22.15.1
+[nodemon] app crashed - waiting for file changes before starting...
